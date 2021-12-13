ae45a53a672942fc6ba91a091794e2f4a5f1404a
Jordi Martinez <jmartinezg.fp@gmail.com>
2021-12-13 13:06:28 +0100

    Hasta clase 38

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+  title: string = 'Contador App';
+  contador: number = 0;
+
+  acumular(valor:number){
+    this.contador += valor;
+  }
 }
